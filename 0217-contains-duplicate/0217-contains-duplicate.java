a00100e63ff10681bcac8a51be3e913e3a42f28b
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-14 23:03:47 +0530

    Time: 25 ms (17.98%), Space: 81.3 MB (93.48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int left=0;
+        int right=1;
+        while(right<nums.length){
+            if(nums[left]==nums[right]){
+                return true;
+            }
+            left++;
+            right++;
+
+        }
+        return false;
+    }
+}
